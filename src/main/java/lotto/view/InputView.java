d1c23b4ff1d8994029d9e9580e3478d968601fd5
YongJun <yj9107vv@gmail.com>
2025-10-30 14:56:12 +0900

    reafactor(view/InputView): 불필요한 readLine() 메서드 제거
    - 직접 호출해서 사용하게 바꿈

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,12 +5,8 @@ import lotto.message.InputMessage;
 
 public class InputView {
 
-    private String readLine() {
-        return Console.readLine();
-    }
-
     public String askPurchaseAmount() {
         System.out.println(InputMessage.PURCHASE_AMOUNT.getMessage());
-        return readLine();
+        return Console.readLine();
     }
 }
