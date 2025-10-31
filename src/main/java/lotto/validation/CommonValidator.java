19d80c21cfdb4907adb546bb040646037825428c
YongJun <yj9107vv@gmail.com>
2025-10-31 14:59:19 +0900

    feat(validation/CommonValidator): validateNumeric() 추가 및 예외 메시지 수정
    - 입력값이 숫자인 검증하는 validateNumeric() 메서드 추가
    - 예외를 던질 때 나타내는 메시지를 예외 클래스에서 정하는 것이 아닌 호출 쪽에서 정하도록 관리(예외 메시지 다양성)

--- src/main/java/lotto/validation/CommonValidator.java
+++ src/main/java/lotto/validation/CommonValidator.java
@@ -4,13 +4,22 @@ import org.junit.platform.commons.util.StringUtils;
 
 public final class CommonValidator {
 
+    private final static String NUMERIC_PATTERN = "\\d+";
+
     private CommonValidator() {
         throw new AssertionError("Cannot instantiate CommonValidator");
     }
 
-    public static void validateBlank(String input) {
+    public static void validateBlank(String input, String message) {
         if (StringUtils.isBlank(input)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(message);
         }
     }
+
+    public static void validateNumeric(String input, String message) {
+        if (!input.equals(NUMERIC_PATTERN)) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
 }
