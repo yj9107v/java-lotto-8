c90bb81c5bd0a574c2336a2aed8d54de70ac44d6
YongJun <yj9107vv@gmail.com>
2025-10-31 16:22:18 +0900

    feat(domain/Policy): 요구사항의 규칙들을 상수 관리를 위한 Polict 클래스 추가
    - 로또 가격, 로또 숫자 범위, 로또 개수를 상수 선언

---
+++ src/main/java/lotto/domain/Policy.java
@@ -0,0 +1,10 @@
+package lotto.domain;
+
+public final class Policy {
+    private Policy() {};
+
+    public final static int LOTTO_PRICE = 1_000;
+    public static final int NUMBER_COUNT = 6;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+}
