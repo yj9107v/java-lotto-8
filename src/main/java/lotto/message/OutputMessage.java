5afbc6312e631cc5b6c4b5613677992cb45464f6
YongJun <yj9107vv@gmail.com>
2025-11-01 16:45:03 +0900

    feat(message/OutputMessage): 출력 메시지 enum 클래스 및 구매 결과 출력 메시지 추가

---
+++ src/main/java/lotto/message/OutputMessage.java
@@ -0,0 +1,16 @@
+package lotto.message;
+
+public enum OutputMessage {
+
+    PURCHASE_RESULT("%d개를 구매했습니다.");
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
