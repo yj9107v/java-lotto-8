d8d5959d562a211a398451e3a28f3431226f28cf
YongJun <yj9107vv@gmail.com>
2025-11-01 20:47:07 +0900

    feat(message/ExceptionMessage): 로또 번호 검증 예외 메시지 추가
    - LottoNumberValidator에 사용되는 예외 메시지 3개 추가
    - 6개, 1 ~ 45 범위, 중복인지

--- src/main/java/lotto/message/ExceptionMessage.java
+++ src/main/java/lotto/message/ExceptionMessage.java
@@ -2,10 +2,12 @@ package lotto.message;
 
 public enum ExceptionMessage {
 
-    INPUT_EMPTY("[ERROR] 입력값이 비어있습니다.\n"),
-    NOT_NUMERIC("[ERROR] 입력값은 숫자여야 합니다.(0, 음수, 공백 X)\n"),
-    PURCHASE_AMOUNT_FORMAT("[ERROR] 구입금액은 1,000원 단위로 입력해야 합니다.\n"),
-    INVALID_LOTTO_NUMBER_COUNT("[ERROR] 로또 번호는 6개여야 합니다.");
+    INPUT_EMPTY("[ERROR] 입력값이 비어있습니다."),
+    NOT_NUMERIC("[ERROR] 입력값은 숫자여야 합니다.(0, 음수, 공백 X)"),
+    PURCHASE_AMOUNT_FORMAT("[ERROR] 구입금액은 1,000원 단위로 입력해야 합니다."),
+    INVALID_LOTTO_NUMBER_COUNT("[ERROR] 로또 번호는 6개여야 합니다."),
+    DUPLICATED_LOTTO_NUMBER("[ERROR] 로또 번호는 중복될 수 없습니다."),
+    OUT_OF_RANGE_NUMBER("[ERROR] 로또 번호는 1부터 45 사이여야 합니다.");
 
     private final String message;
 
