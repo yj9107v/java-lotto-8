174e1442d950b546c43b509c736d800052a80c1e
YongJun <yj9107vv@gmail.com>
2025-11-01 17:34:38 +0900

    feat(utils/TextFormat): 예외 종류 변경
    - IllegalStateException()를  AssertionError()로 변경
    - 객체 상태가 없기에 논리적 불변 위반에 더 어울리는 예외로 변경

--- src/main/java/lotto/utils/TextFormat.java
+++ src/main/java/lotto/utils/TextFormat.java
@@ -5,6 +5,6 @@ public final class TextFormat {
     public static final String LINE_SEPARATOR = System.lineSeparator();
 
     private TextFormat() {
-        throw new IllegalStateException("Utility class");
+        throw new AssertionError("No instances of TextFormat");
     }
 }
