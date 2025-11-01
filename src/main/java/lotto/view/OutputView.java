6598f6619d780f4724d791d58c93fea635ebe081
YongJun <yj9107vv@gmail.com>
2025-11-01 21:57:48 +0900

    feat(view/OutputView): 발행한 로또 수량 및 번호를 출력하는 printPurchaseResult() 추가
    - 로또 수량 + 로또 번호 출력은 하나의 책임이기에 한 블록으로 묶음
    - LottoTickets tickets를 전달받아 로또 수량과 로또 번호 목록을 반환해 출력한다

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,12 @@
 package lotto.view;
 
+import lotto.domain.LottoTickets;
 import lotto.message.OutputMessage;
 
 public class OutputView {
 
-    public void printPurchaseResult(int ticketCount) {
-        System.out.printf(OutputMessage.PURCHASE_RESULT.getMessage(), ticketCount);
+    public void printPurchaseResult(LottoTickets tickets) {
+        System.out.printf(OutputMessage.PURCHASE_COUNT_MESSAGE.getMessage(), tickets.size());
+        tickets.values().forEach(System.out::println);
     }
 }
