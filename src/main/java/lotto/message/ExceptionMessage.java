95ed3233ca90e3594c3acf92a469673af3603a44
YongJun <yj9107vv@gmail.com>
2025-11-01 16:49:19 +0900

    feat(message/ExceptionMessage): 예외 메시지 2개 추가
    - 1,000원 단위 입력 예외 메시지 추가
    - "로또 번호는 6개여야 한다" 예외 메시지 추가

--- src/main/java/lotto/message/ExceptionMessage.java
+++ src/main/java/lotto/message/ExceptionMessage.java
@@ -2,8 +2,10 @@ package lotto.message;
 
 public enum ExceptionMessage {
 
-    INPUT_EMPTY("[ERROR] 입력값이 비어있습니다."),
-    NOT_NUMERIC("[ERROR] 입력값은 숫자여야 합니다.");
+    INPUT_EMPTY("[ERROR] 입력값이 비어있습니다.\n"),
+    NOT_NUMERIC("[ERROR] 입력값은 숫자여야 합니다.(0, 음수, 공백 X)\n"),
+    PURCHASE_AMOUNT_FORMAT("[ERROR] 구입금액은 1,000원 단위로 입력해야 합니다.\n"),
+    INVALID_LOTTO_NUMBER_COUNT("[ERROR] 로또 번호는 6개여야 합니다.");
 
     private final String message;
 
