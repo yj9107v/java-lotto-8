b9c93afbe013235965468a98c410a79c60ff14da
YongJun <yj9107vv@gmail.com>
2025-11-01 20:42:13 +0900

    refactor(domain/Lotto): 기존 validate()를 LottoNumberValidator 정적 메서드로 변경
    - 성공 번호 검증과 같아 기존 validate() 내용을 공유하기 위해 LottoNumberValidator로 이동하여 사용
    - validateLottoNumbers()를 통해 로또 숫자 번호 검증
    - 로또 번호가 6개인지, 1 ~ 45 범위, 중복된 숫자가 아닌지 검증한다
    - Lotto는 불변 객체이기 때문에 방어적 복사 List.copyOf()를 사용

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,25 +1,18 @@
 package lotto.domain;
 
 import java.util.List;
-import lotto.message.ExceptionMessage;
+import lotto.validation.LottoNumberValidator;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     private Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+        LottoNumberValidator.validateLottoNumbers(numbers);
+        this.numbers = List.copyOf(numbers);
     }
 
     public static Lotto of(List<Integer> numbers) {
         return new Lotto(numbers);
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != Policy.NUMBER_COUNT) {
-            throw new IllegalArgumentException(ExceptionMessage.INVALID_LOTTO_NUMBER_COUNT.getMessage());
-        }
-    }
-
-    // TODO: 추가 기능 구현
 }
