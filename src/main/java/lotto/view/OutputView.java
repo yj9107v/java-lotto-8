24dd5214b2c391f2c06d059b0419723ccc2583c4
YongJun <yj9107vv@gmail.com>
2025-11-02 12:30:25 +0900

    refactor(view/OutputView): 출력 메시지 format() 메서드 활용
    - printf()를 사용하지 않고 String API인 format()을 활용
    - 출력 포맷을 지키기 위해 줄바꿈 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,7 +6,8 @@ import lotto.message.OutputMessage;
 public class OutputView {
 
     public void printPurchaseResult(LottoTickets tickets) {
-        System.out.printf(OutputMessage.PURCHASE_COUNT_MESSAGE.getMessage(), tickets.size());
+        System.out.println(OutputMessage.PURCHASE_COUNT_MESSAGE.format(tickets.size()));
         tickets.values().forEach(System.out::println);
+        System.out.println();
     }
 }
