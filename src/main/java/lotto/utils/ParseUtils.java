2df40ab0782c23b16ca5bff05206295aada5b096
YongJun <yj9107vv@gmail.com>
2025-11-02 15:33:35 +0900

    feat(utils/ParseUtils): 의도에 맞게 클래스명 ParseUtils로 변경 및 parseWinningNumbers() 메서드 추가
    - Controller에서 의도 파악할 수 있게 일관된 네이밍 패턴 추가 -> parsePurhcaseAmount(), parseWinningNumbers()
    - 위 두 함수는 ParseUtils 클래스의 함수들을 조합해서 사용
    - 이전 Parsed 클래스명은 의도가 달라 ParseUtils 클래스명으로 변경
    - ParseUtils 클래스에만 사용하는 검증 메서드가 CommonValidator에 있어 옮겨옴 -> 외부 클래스가 사용하게 되면 다시 공용 예외로 리팩터링
    - 로또 당첨 번호 구분자 분리 메서드 splitByDelimiterStrict() 추가
    - parseWinningNumbers()는 splitByDelimiterStrict()로 입력 거믖ㅇ과 구분자 분리 후 parseIntStrict() 숫자 변환과 검증 후 List<Integer>로 변환 해주는 메서드

---
+++ src/main/java/lotto/utils/ParseUtils.java
@@ -0,0 +1,49 @@
+package lotto.utils;
+
+import java.util.List;
+import lotto.message.ExceptionMessage;
+import org.junit.platform.commons.util.StringUtils;
+
+public final class ParseUtils {
+
+    private static final String DELIMITER = ",";
+    private static final String NUMERIC_PATTERN = "[1-9]\\d*";
+
+
+    private ParseUtils() {
+        throw new AssertionError(ExceptionMessage.CANNOT_INSTANCE_CLASS.format(ParseUtils.class.getName()));
+    }
+
+    public static int parsePurchaseAmount(String input) {
+        return parseIntStrict(input);
+    }
+
+    public static List<Integer> parseWinningNumbers(String input) {
+        return splitByDelimiterStrict(input).stream()
+                .map(ParseUtils::parseIntStrict)
+                .toList();
+    }
+
+    public static int parseIntStrict(String input) {
+        validateBlank(input);
+        validatePositiveNumeric(input, ExceptionMessage.NOT_NUMERIC.getMessage());
+        return Integer.parseInt(input);
+    }
+
+    public static List<String> splitByDelimiterStrict(String input) {
+        validateBlank(input);
+        return List.of(input.split(DELIMITER));
+    }
+
+    private static void validateBlank(String input) {
+        if (StringUtils.isBlank(input)) {
+            throw new IllegalArgumentException(ExceptionMessage.INPUT_EMPTY.getMessage());
+        }
+    }
+
+    private static void validatePositiveNumeric(String input, String message) {
+        if (!input.matches(NUMERIC_PATTERN)) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+}
