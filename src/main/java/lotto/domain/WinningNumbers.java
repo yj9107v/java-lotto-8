183d7dc753998d25133878a012588d97e5f3fd24
YongJun <yj9107vv@gmail.com>
2025-11-02 20:22:14 +0900

    refactor(domain/WinningNumbers): 정적 팩토리 메서드에서의 검증을 생성자에서 검증하도록 수정
    - 생성자 내부에서 검증을 해야 생성할 시 유효한 상태를 유지하기 좋기 때문에 수정
    - of() 메서드는 생성 표현만 단순하게 유지하는 것이 좋음

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -8,11 +8,11 @@ public class WinningNumbers {
     private final List<Integer> numbers;
 
     private WinningNumbers(List<Integer> numbers) {
+        LottoNumberValidator.validate(numbers);
         this.numbers = List.copyOf(numbers);
     }
 
     public static WinningNumbers of(List<Integer> numbers) {
-        LottoNumberValidator.validate(numbers);
         return new WinningNumbers(numbers);
     }
 
