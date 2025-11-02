a7edc13ff8198f70b85e289fb80779d872015c27
YongJun <yj9107vv@gmail.com>
2025-11-02 17:45:40 +0900

    refactor(message/ExceptionMessage): NOT_NUMERIC 변수명 수정
    - 숫자 검증 예외 메시지를 공유하기 위해 PURCHASE_AMOUNT_NOT_NUMERIC을 NOT_NUMERIC으로 변경
    - 구입금액 입력과 보너스 번호의 숫자 검증 예외 메시지가 동일하기 때문

--- src/main/java/lotto/message/ExceptionMessage.java
+++ src/main/java/lotto/message/ExceptionMessage.java
@@ -4,7 +4,7 @@ public enum ExceptionMessage {
 
     CANNOT_INSTANCE_CLASS("[ERROR] %s 클래스는 인스턴스화 할 수 없습니다. "),
     INPUT_EMPTY("[ERROR] 입력값이 비어있습니다."),
-    PURCHASE_AMOUNT_NOT_NUMERIC("[ERROR] 입력값은 숫자여야 합니다.(0, 음수, 공백 X)"),
+    NOT_NUMERIC("[ERROR] 입력값은 숫자여야 합니다.(0, 음수, 공백 X)"),
     PURCHASE_AMOUNT_FORMAT("[ERROR] 구입금액은 1,000원 단위로 입력해야 합니다."),
     WINNING_NUMBERS_NOT_NUMERIC("[ERROR] 당첨 번호는 숫자와 구분자(,)로 이루어져야 합니다.(0, 음수, 공백 X)"),
     INTEGER_OVERFLOW("[ERROR] 입력값이 정수의 최댓값을 초과했습니다."),
