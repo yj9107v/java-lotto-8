20b7b47575df8d4b286e889e5c4abf9fd39f6f95
YongJun <yj9107vv@gmail.com>
2025-11-03 15:27:38 +0900

    feat(domain/Rank): 당첨 통계 출력을 위한 등급 별 메시지 추가
    - 당첨 통계만 출력하므로 MISS는 출력 안 하도록 isWinning() 메서드 추가
    - format() 메서드로 메시지와 개수를 포맷

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -2,25 +2,23 @@ package lotto.domain;
 
 public enum Rank {
 
-    FIRST(6, false, 2_000_000_000L),
-    SECOND(5, true, 30_000_000L),
-    THIRD(5, false, 1_500_000L),
-    FOURTH(4, false, 50_000L),
-    FIFTH(3, false, 5_000L),
-    MISS(0, false, 0L);
+    MISS(0, false, 0L, ""),
+    FIFTH(3, false, 5_000L, "3개 일치 (5,000원) - %d개"),
+    FOURTH(4, false, 50_000L, "4개 일치 (50,000원) - %d개"),
+    THIRD(5, false, 1_500_000L, "5개 일치 (1,500,000원) - %d개"),
+    SECOND(5, true, 30_000_000L, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    FIRST(6, false, 2_000_000_000L, "6개 일치 (2,000,000,000원) - %d개");
 
     private final int matchCount;
     private final boolean bonusRequired;
     private final long prize;
+    private final String message;
 
-    Rank(int matchCount, boolean bonusRequired, long prize) {
+    Rank(int matchCount, boolean bonusRequired, long prize, String message) {
         this.matchCount = matchCount;
         this.bonusRequired = bonusRequired;
         this.prize = prize;
-    }
-
-    public long getPrize() {
-        return prize;
+        this.message = message;
     }
 
     public static Rank of(int matchCount, boolean bonusMatched) {
@@ -30,5 +28,17 @@ public enum Rank {
         if (matchCount == 4) return FOURTH;
         if (matchCount == 3) return FIFTH;
         return MISS;
+    }
+
+    public long getPrize() {
+        return prize;
+    }
+
+    public boolean isWinning() {
+        return this != MISS;
+    }
+
+    public String format(Object... args) {
+        return String.format(message, args);
     }
 }
