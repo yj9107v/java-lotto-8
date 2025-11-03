8dea8f7bec3d4c78995bd4f058f4d8d01edad9ff
YongJun <yj9107vv@gmail.com>
2025-11-03 13:11:47 +0900

    feat(domain/Winning): 로또 당첨 번호와 보너스 번호 객체를 관리하는 Winning 클래스 추가
    - WinningNumbers, BonusNumber 불변 객체를 정적 팩토리 메서드를 통해 입력받아 객체 생성
    - 객체 생성 내에서 두 값이 중복됐는지 검증, 됐다면 예외을 던진다
    - 불변 객체들을 반환하는 메서드 이름은 각 객체 이름과 동일, 이유는 VO를 반환하는 역할이기 때문

---
+++ src/main/java/lotto/domain/Winning.java
@@ -0,0 +1,43 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.message.ExceptionMessage;
+
+public class Winning {
+
+    private final WinningNumbers winningNumbers;
+    private final BonusNumber bonusNumber;
+
+    private Winning(WinningNumbers winningNumbers, BonusNumber bonusNumber) {
+        validateNoDuplicate(winningNumbers, bonusNumber);
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static Winning of(WinningNumbers winningNumbers, BonusNumber bonusNumber) {
+        return new Winning(winningNumbers, bonusNumber);
+    }
+
+    public void validateNoDuplicate(WinningNumbers winningNumbers, BonusNumber bonusNumber) {
+        if (winningNumbers.values().contains(bonusNumber.value())) {
+            throw new IllegalArgumentException(ExceptionMessage.WINNING_NUMBERS_NOT_NUMERIC.getMessage());
+        }
+    }
+
+    public WinningNumbers winningNumbers() {
+        return winningNumbers;
+    }
+
+    public BonusNumber bonusNumber() {
+        return bonusNumber;
+    }
+
+    public Rank RankOf(Lotto ticket) {
+        List<Integer> win = List.copyOf(winningNumbers.values());
+        int match = (int) ticket.values().stream().filter(win::contains).count();
+        boolean bonusMatch = ticket.values().contains(bonusNumber.value());
+        return Rank.of(match, bonusMatch);
+    }
+
+
+}
