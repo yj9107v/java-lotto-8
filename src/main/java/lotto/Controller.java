ef5041f1f954b5c5ea0b6c4deb75d6fe3d8e52ec
YongJun <yj9107vv@gmail.com>
2025-11-03 17:50:33 +0900

    refactor(controller): 중복 로직 제거 및 기능 별 메서드 분리
    - (while-try-catch)를 입력 재시도 로직을 공통화를 통해 중복 로직 제거
    - 기능 별 메서드 분리

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -1,6 +1,5 @@
 package lotto;
 
-import java.util.List;
 import lotto.domain.BonusNumber;
 import lotto.domain.LottoMachine;
 import lotto.domain.LottoTickets;
@@ -11,6 +10,7 @@ import lotto.domain.WinningNumbers;
 import lotto.domain.WinningStatistics;
 import lotto.infra.MissionUtilsLottoNumberGenerator;
 import lotto.utils.ParseUtils;
+import lotto.utils.Retrier;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -18,56 +18,82 @@ public class Controller {
 
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
-    private PurchaseAmount purchaseAmount;
-    private WinningNumbers winningNumbers;
-    private BonusNumber bonusNumber;
-    private Winning winning;
 
     public void run() {
-        while (true) {
-            try {
-                String inputPurchaseAmount = inputView.askPurchaseAmount();
-                int rawPurchaseAmount = ParseUtils.parsePurchaseAmount(inputPurchaseAmount);
-                purchaseAmount = PurchaseAmount.of(rawPurchaseAmount);
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.printf("%n%s%n%n", e.getMessage());
-            }
-        }
+        // 1) 구입금액 입력
+        PurchaseAmount purchaseAmount = readPurchaseAmount();
 
-        // 도메인 별 나누기??
-        LottoTickets tickets = LottoTickets.of(
+        // 2) 로또 발행 및 발행한 로또 수량 및 번호 출력
+        LottoTickets tickets = showIssueTickets(purchaseAmount);
+
+        // 3) 당첨 번호 입력
+        WinningNumbers winningNumbers = readWinningNumbers();
+
+        // 4) 보너스 번호 입력 및 (당첨, 보너스 번호) 관리 및 중첩 여부 확인
+        Winning winning = createWinningWithBonusNumber(winningNumbers);
+
+        // 5) 당첨 통계 계산 및 출력
+        WinningStatistics winningStatistics = showWinningStatistics(winning, tickets);
+
+        // 6) 수익률 계산 및 출력
+        showProfitRate(winningStatistics, purchaseAmount);
+    }
+
+    private PurchaseAmount readPurchaseAmount() {
+        return Retrier.untilValid(
+                () -> PurchaseAmount.of(ParseUtils.parsePurchaseAmount(inputView.askPurchaseAmount())), outputView);
+    }
+
+    private LottoTickets showIssueTickets(PurchaseAmount purchaseAmount) {
+        LottoTickets tickets = issueTickets(purchaseAmount);
+        printLottoTickets(tickets);
+        return tickets;
+    }
+
+    private static LottoTickets issueTickets(PurchaseAmount purchaseAmount) {
+        return LottoTickets.of(
                 LottoMachine.withGenerator(new MissionUtilsLottoNumberGenerator())
                         .issueTickets(purchaseAmount.toTicketCount()));
+    }
+
+    private void printLottoTickets(LottoTickets tickets) {
         outputView.printPurchaseResult(tickets);
+    }
 
-        while (true) {
-            try {
-                String inputWinningNumbers = inputView.askWinningNumbers();
-                List<Integer> rawWinningNumbers = ParseUtils.parseWinningNumbers(inputWinningNumbers);
-                winningNumbers = WinningNumbers.of(rawWinningNumbers);
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.printf("%n%s%n", e.getMessage());
-            }
-        }
+    private WinningNumbers readWinningNumbers() {
+        return Retrier.untilValid(
+                () -> WinningNumbers.of(ParseUtils.parseWinningNumbers(inputView.askWinningNumbers())), outputView);
+    }
 
-        while (true) {
-            try {
-                String inputBonusNumber = inputView.askBonusNumber();
-                int rawBonusNumber = ParseUtils.parseBonusNumber(inputBonusNumber);
-                bonusNumber = BonusNumber.of(rawBonusNumber);
-                winning = Winning.of(winningNumbers, bonusNumber);
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.printf("%n%s%n", e.getMessage());
-            }
-        }
+    private Winning createWinningWithBonusNumber(WinningNumbers winningNumbers) {
+        return Retrier.untilValid(() -> Winning.of(winningNumbers,
+                BonusNumber.of(ParseUtils.parseBonusNumber(inputView.askBonusNumber()))), outputView);
+    }
 
-        WinningStatistics winningStatistics = WinningStatistics.from(winning, tickets);
+    private WinningStatistics showWinningStatistics(Winning winning, LottoTickets tickets) {
+        WinningStatistics winningStatistics = calculateWinningStatistics(winning, tickets);
+        getPrintWinningStatistics(winningStatistics);
+        return winningStatistics;
+    }
+
+    private static WinningStatistics calculateWinningStatistics(Winning winning, LottoTickets tickets) {
+        return WinningStatistics.from(winning, tickets);
+    }
+
+    private void getPrintWinningStatistics(WinningStatistics winningStatistics) {
         outputView.printWinningStatistics(winningStatistics);
+    }
 
-        ProfitRate profitRate = ProfitRate.of(winningStatistics.totalPrize(), purchaseAmount.value());
+    private void showProfitRate(WinningStatistics winningStatistics, PurchaseAmount purchaseAmount) {
+        ProfitRate profitRate = calculateProfitRate(winningStatistics, purchaseAmount);
+        printProfitRate(profitRate);
+    }
+
+    private static ProfitRate calculateProfitRate(WinningStatistics winningStatistics, PurchaseAmount purchaseAmount) {
+        return ProfitRate.of(winningStatistics.totalPrize(), purchaseAmount.value());
+    }
+
+    private void printProfitRate(ProfitRate profitRate) {
         outputView.printProfitRate(profitRate.value());
     }
 }
