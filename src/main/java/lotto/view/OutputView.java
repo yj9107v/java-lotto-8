4639ba2fab839577baad5c438a164dd6ecbfaf95
YongJun <yj9107vv@gmail.com>
2025-11-03 15:30:50 +0900

    feat(view/OutputView): 당첨 통계 출력 메서드 printWinningStatistics() 추가 및 메서드 분리
    - 요구사항에 맞는 당첨 통계 출력 메시지 구현
    - WinningStatistics 객체를 받아 등급 별 개수를 가져와 Rank 별 개수 출력
    - 메서드 역할 별 기능 분리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,13 +1,41 @@
 package lotto.view;
 
+import java.util.Arrays;
 import lotto.domain.LottoTickets;
+import lotto.domain.Rank;
+import lotto.domain.WinningStatistics;
 import lotto.message.OutputMessage;
 
 public class OutputView {
 
+
     public void printPurchaseResult(LottoTickets tickets) {
-        System.out.println(OutputMessage.PURCHASE_COUNT_MESSAGE.format(tickets.size()));
+        printPurchaseHeader(tickets);
+        printTickets(tickets);
+    }
+
+    public void printWinningStatistics(WinningStatistics winningStatistics) {
+        printWinningHeaderWithSeparator();
+        printRanks(winningStatistics);
+    }
+
+    private void printPurchaseHeader(LottoTickets tickets) {
+        System.out.printf("%n%s%n", OutputMessage.PURCHASE_COUNT_MESSAGE.format(tickets.size()));
+    }
+
+    private void printTickets(LottoTickets tickets) {
         tickets.values().forEach(System.out::println);
-        System.out.println();
+    }
+
+    private void printWinningHeaderWithSeparator() {
+        System.out.printf("%n%s%n%s%n", OutputMessage.WINNING_STATISTICS_MESSAGE.getMessage(),
+                OutputMessage.SEPARATOR.getMessage());
+    }
+
+    private void printRanks(WinningStatistics winningStatistics) {
+        Arrays.stream(Rank.values())
+                .filter(Rank::isWinning)
+                .map(rank -> rank.format(winningStatistics.countOf(rank)))
+                .forEach(System.out::println);
     }
 }
